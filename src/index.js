6791647ba39ec96d1c12de2f129ed0c0582395ca
Hovhannhes Serobyan <hovo.serobyan.85@gmail.com>
2022-06-26 15:31:31 +0400

    init: start reverse-init task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    num = n + '';
+    let reversedText = num.split('').reverse().join('');
+    let reversedNumber = parseInt(reversedText, 10);
+    return reversedNumber;
 }
